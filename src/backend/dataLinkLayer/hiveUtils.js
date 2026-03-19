24fe1ddacd9b6fa3f51931fdd919242595ad3f78
student <rch3141@g.rit.edu>
2026-03-19 17:41:59 -0400

    added some more DLL functions

--- src/backend/dataLinkLayer/hiveUtils.js
+++ src/backend/dataLinkLayer/hiveUtils.js
@@ -1,5 +1,11 @@
 import * as dbutils from "./dbutils.js";
 
+/**
+ * Returns the starttime for the given hiveID
+ * 
+ * @param {*} hiveID 
+ * @returns the startime
+ */
 async function getStartTime(hiveID){
     let data = await dbutils.getData('hive',['startdate'],{hiveID : hiveID});
 
@@ -7,10 +13,22 @@ async function getStartTime(hiveID){
     return data.rows[0];
 }
 
-getStartTime(1)
+// getStartTime(1)
 
+/**
+ * Tests the given passkey and returns the hiveID if valid or -1 if invalid
+ * 
+ * @param {*} passkey 
+ * @returns the hiveID if valid or -1 if invalid
+ */
 async function testPasskey(passkey){
     let data = await dbutils.getData('hive',['hiveid'],{passkey: passkey});
-
-    console.log(data)
+    
+    //return hiveID if exists
+    if(parseInt(data['rowCount']) == 1){
+        return data['rows'][0]['hiveid'];
+    }
+    else{
+        return -1;
+    }
 }
